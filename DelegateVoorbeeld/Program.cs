7b1abbce4b3999462d4f29d2286fbcb869206c23
Frederik Thuysbaert <fthuysbaert@gmail.com>
2015-10-06 12:03:23 +0200

    test

---
+++ DelegateVoorbeeld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateVoorbeeld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = new Logger();
+            Writer wr = new Writer(log.writeMessage);
+            wr("Hello PXL");
+        }
+    }
+}
---
+++ DelegateVoorbeeld/Writer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateVoorbeeld
+{
+    public delegate void Writer(string message);
+    
+}
